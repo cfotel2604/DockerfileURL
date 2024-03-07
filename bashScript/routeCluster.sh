15a5f9d8bb1211916a34d3636b9dd1718d1c71de
cfotel <91402827+cfotel2604@users.noreply.github.com>
2024-03-08 06:29:08 +0700

    Update routeCluster.sh
    change dev eth0 to special gw

--- bashScript/routeCluster.sh
+++ bashScript/routeCluster.sh
@@ -4,4 +4,4 @@
 #set -ex
 route del -net 10.244.0.0/16 ; route add -net 10.244.0.0/16 dev eth0
 route del -net 103.15.49.0/24 ; route add -net 103.15.49.0/24 dev eth0
-route del -net 0.0.0.0 dev eth0
+route del -net 0.0.0.0 gw 169.254.1.1
