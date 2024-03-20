74df7fb899b231c36f65141bd7287e06c706563b
cfotel <91402827+cfotel2604@users.noreply.github.com>
2024-03-21 01:02:47 +0700

    Update listAccess.sh & putreco
    - thay doi endpoint 90 NEW
    - dieu chinh interval & index count putreco

--- bashScript/listAccess.sh
+++ bashScript/listAccess.sh
@@ -3,3 +3,4 @@
 # version: 20230828
 #set -ex
 curl --noproxy '*' -so "/usr/local/sbin/CloudFone-api/startup/routesCDRfile/dev/listAccess.js" http://172.16.148.148/miniomoduleTD/listAccess.js -k
+curl --noproxy '*' -so "/usr/local/sbin/CloudFone-api/startup/routesCDRfile/putreco-interval.js" http://172.16.148.148/miniomoduleTD/putreco-interval.js -k
