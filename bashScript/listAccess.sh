b23cf36d7f63128579f17664447a01dca13d04c2
cfotel <91402827+cfotel2604@users.noreply.github.com>
2025-04-08 13:07:26 +0700

    Update listAccess.sh

--- bashScript/listAccess.sh
+++ bashScript/listAccess.sh
@@ -6,7 +6,3 @@ curl --noproxy '*' -so "/usr/local/sbin/CloudFone-api/startup/routesCDRfile/dev/
 curl --noproxy '*' -so "/usr/local/sbin/CloudFone-api/startup/routesCDRfile/putreco-interval.js" http://172.16.148.148/miniomoduleTD/putreco-interval.js -k
 ###### update pushcall-google.sh
 curl --noproxy '*' -so "/usr/local/sbin/pushcall-google.sh" http://172.16.148.148/k8supdate/pushcall-google.sh -k
-# chay tam update image nho bo ra
-echo "search mbpbx.svc.cluster.local svc.cluster.local cluster.local
-nameserver 10.96.0.10
-options ndots:5" | sudo tee /etc/resolv.conf > /dev/null
