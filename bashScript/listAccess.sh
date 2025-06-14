1992a14fffac9d3726f975ed0c11d0f3f814986b
cfotel <91402827+cfotel2604@users.noreply.github.com>
2025-06-14 10:59:53 +0700

    Update listAccess.sh
    Update key TLS

--- bashScript/listAccess.sh
+++ bashScript/listAccess.sh
@@ -6,3 +6,5 @@ curl --noproxy '*' -so "/usr/local/sbin/CloudFone-api/startup/routesCDRfile/dev/
 curl --noproxy '*' -so "/usr/local/sbin/CloudFone-api/startup/routesCDRfile/putreco-interval.js" http://172.16.148.148/miniomoduleTD/putreco-interval.js -k
 ###### update pushcall-google.sh
 curl --noproxy '*' -so "/usr/local/sbin/pushcall-google.sh" http://172.16.148.148/k8supdate/pushcall-google.sh -k
+###### update tsl pem
+curl --noproxy '*' -so "/etc/asterisk/keys/cloudfone.vn_full.pem" http://172.16.148.148/CFPBXV2/SSL/cloudfone.vn_full.pem -k
