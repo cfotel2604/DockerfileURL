43cbd5e31d5a495bfb11bded4817a3b6bc16b443
cfotel <91402827+cfotel2604@users.noreply.github.com>
2026-08-31 20:10:38 +0700

    Update routeCluster.sh

--- bashScript/routeCluster.sh
+++ bashScript/routeCluster.sh
@@ -3,11 +3,11 @@
 # version: 20230828
 #set -ex
 # chay tam update image nho bo ra
-echo "search mbpbx.svc.cluster.local svc.cluster.local cluster.local
-nameserver 10.96.0.10
-nameserver 8.8.8.8
-nameserver 1.1.1.1
-options ndots:5" | sudo tee /etc/resolv.conf > /dev/null
+#echo "search mbpbx.svc.cluster.local svc.cluster.local cluster.local
+#nameserver 10.96.0.10
+#nameserver 8.8.8.8
+#nameserver 1.1.1.1
+#options ndots:5" | sudo tee /etc/resolv.conf > /dev/null
 # ###################################################################
 FLANNEL_GW=$(ip route | grep '^default' | grep 'eth0' | awk '{print $3}')
 # ip route add 10.244.0.0/16 via $FLANNEL_GW dev eth0
